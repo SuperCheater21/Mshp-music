722474584d9200db91d60887b07a39c149df0c76
Dmitry Leonov <dima.leonov.2008@mail.ru>
2024-03-22 19:50:04 +0300

    0.1 version      p.s.start to work on it now!

--- Playlists/models.py
+++ Playlists/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from Songs.models import Song
 
+class Genre(models.Model):
+    name = models.TextField()
 
+class Playlist(models.Model):
+    title = models.TextField()
+    author = models.TextField()
+    image = models.ImageField()
+    songs = models.ManyToManyField(Song)
+    genre = models.OneToOneField(Genre,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- Songs/views.py
+++ Songs/views.py
@@ -9,3 +9,8 @@ from django.contrib.auth.decorators import  login_required
 from django.contrib.auth import authenticate, login, logout
 
 
+def music_page(request):
+    song = Song(title='Paranoid Android', artist='Radiohead')
+    song.save()
+    context = {'song': song}
+    return render(request, 'music_page.html', context)
