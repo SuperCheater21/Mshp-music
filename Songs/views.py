6796715d095d9ce16ae39281ad39fba4284ccb64
Jagtrieb <Boegor3105@gmail.com>
2024-05-03 20:32:46 +0300

    Fixing problems with lyrics and next and prev buttons

--- Songs/views.py
+++ Songs/views.py
@@ -20,9 +20,28 @@ def song_list(request):
 
 def play_song(request, song_id):
     song = get_object_or_404(Song, pk=song_id)  # Get song by ID
-    # Logic to handle song playback using JavaScript or a third-party player library
-    # (implementation details depend on your chosen approach)
+    first_element = Song.objects.order_by('id').first().id
+    last_element = Song.objects.order_by('id').last().id
+    next_element = song_id + 1 if song_id < last_element else first_element
+    prev_element = song_id - 1 if song_id > first_element else last_element
     context = {'song': song,
-               'next_song_id': song_id + 1,
-               'prev_song_id': song_id - 1}
+               'next_song_id': next_element,
+               'prev_song_id': prev_element,
+               }
     return render(request, 'play_song.html', context)
+
+
+# def check_element_position(element_id):
+#     try:
+#         element = Element.objects.get(pk=element_id)
+#         first_element = Element.objects.order_by('id').first()
+#         last_element = Element.objects.order_by('id').last()
+#
+#         if element == first_element:
+#             print("Это первый элемент в базе данных")
+#         elif element == last_element:
+#             print("Это последний элемент в базе данных")
+#         else:
+#             print("Это не первый и не последний элемент в базе данных")
+#     except Element.DoesNotExist:
+#         print("Элемент с таким ID не найден")
