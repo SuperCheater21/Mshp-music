954f1a3491acd84933d428b6935f909006076c3c
Jagtrieb <Boegor3105@gmail.com>
2024-05-20 20:54:54 +0300

    added simple frontend

--- Songs/views.py
+++ Songs/views.py
@@ -21,10 +21,10 @@ from django.db import IntegrityError
 from django.dispatch import receiver
 
 
-
 def song_list(request):
     songs = Song.objects.all()  # Fetch all songs
     return render(request, 'music/song_list.html', {'songs': songs})
+
 
 @login_required(login_url='login')
 def play_song_by_slug(request, song_id):
@@ -44,14 +44,14 @@ def play_song_by_slug(request, song_id):
         else:
             is_liked = False
 
-        context = {'song': song, 'is_your_song':  is_your_song, "is_liked": is_liked}
+        context = {'song': song, 'is_your_song': is_your_song, "is_liked": is_liked}
         return render(request, 'play_song_by_slug.html', context)
     except Song.DoesNotExist:
         return render(request, 'not_found.html', {'what': 'song'})
 
 
 @login_required(login_url='login')
-def play_song_in_playlist(request,playlist_id, song_id):
+def play_song_in_playlist(request, playlist_id, song_id):
     try:
         playlist = Playlist.objects.get(slug=playlist_id)
 
@@ -62,25 +62,23 @@ def play_song_in_playlist(request,playlist_id, song_id):
             prev_element = len(playlist.songs.all()) - 1
         if next_element >= len(playlist.songs.all()):
             next_element = 0
-        #print(playlist.songs.all())
+        # print(playlist.songs.all())
         song = playlist.songs.all()[song_id]
 
         context = {'song': song,
-               'prev_song_id':prev_element,
-               'next_song_id':next_element,
-                   'playlist':playlist}
+                   'prev_song_id': prev_element,
+                   'next_song_id': next_element,
+                   'playlist': playlist}
         return render(request, 'play_song_in_playlist.html', context)
     except Playlist.DoesNotExist:
         return render(request, 'not_found.html', {'what': 'playlist'})
-
-
-
 
 
 '''@login_required(login_url='login')
 def music_page(request):
 
     return render(request, 'music/music_page.html')'''
+
 
 @login_required(login_url='login')
 def my_vibe_page(request):
@@ -106,7 +104,6 @@ def my_vibe_page(request):
     for song in prlist.playlists.all()[0].songs.all():
         temp.add(song)
 
-
     recommendation = list(temp)
 
     try:
@@ -117,11 +114,10 @@ def my_vibe_page(request):
         return redirect('../playlist/' + playlist.slug)
 
 
-
 @login_required(login_url='login')
 def upload_song(request, playlist_id):
     if request.method == 'POST':
-        song_form = SongForm(request.POST,request.FILES)
+        song_form = SongForm(request.POST, request.FILES)
 
         if song_form.is_valid():
             new_song = song_form.save(commit=False)
@@ -133,19 +129,19 @@ def upload_song(request, playlist_id):
 
             messages.success(request, 'This song is added successfully')
             song_id = len(playlist.songs.all()) - 1
-            return redirect('song/'  + str(song_id))
+            return redirect('song/' + str(song_id))
     else:
         song_form = SongForm()
 
     return render(request, 'upload_song.html',
                   {'song_form': song_form})
 
+
 @login_required(login_url='login')
 def change_song(request, song_id):
     try:
         song_form = SongForm(request.POST, request.FILES, instance=Song.objects.get(slug=song_id))
         if request.method == 'POST':
-
 
             if song_form.is_valid():
                 song_form.save()
@@ -155,22 +151,22 @@ def change_song(request, song_id):
             song_form = SongForm()
 
         return render(request, 'upload_song.html',
-                  {'song_form': song_form})
+                      {'song_form': song_form})
     except Song.DoesNotExist:
         return render(request, 'not_found.html', {'what': 'song'})
-
 
 
 @login_required(login_url='login')
 def delete_song(request, song_id):
     try:
-        song = Song.objects.get(slug= song_id)
+        song = Song.objects.get(slug=song_id)
         song.delete()
 
         messages.success(request, 'This song has been deleted successfully')
         return redirect(request.path_info)
     except Song.DoesNotExist:
         return render(request, 'not_found.html', {'what': 'song'})
+
 
 @login_required(login_url='login')
 def delete_song_from_playlist(request, playlist_id, song_id):
@@ -183,12 +179,12 @@ def delete_song_from_playlist(request, playlist_id, song_id):
     except Song.DoesNotExist:
         return render(request, 'not_found.html', {'what': 'song'})
 
+
 @login_required(login_url='login')
 def like_song(request, song_id):
     try:
         song = Song.objects.get(slug=song_id)
         prlist = PreferenceList.objects.get(profile=request.user.profile)
-
 
         prlist.playlists.all()[0].songs.add(song)
 
@@ -210,9 +206,3 @@ def unlike_song(request, song_id):
         return redirect('../../song/' + song_id)
     except Song.DoesNotExist:
         return render(request, 'not_found.html', {'what': 'song'})
-
-
-
-
-
-
